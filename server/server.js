5f140f030c2919096fb92ee891bae93e0909f7a5
Inti Velasquez <intiv@unitec.edu>
2018-06-13 20:42:48 -0600

    DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -11,6 +11,9 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+
+//POST /todos
+
 app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
@@ -23,6 +26,9 @@ app.post('/todos', (req, res) => {
     });
 });
 
+
+//GET /todos
+
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
@@ -31,22 +37,36 @@ app.get('/todos', (req, res) => {
     }).catch((e) => console.log(e));
 });
 
-
-//GET
-
 app.get('/todos/:id', (req, res) => {
     // res.send(req.params);
     let id = req.params.id;
     if(!ObjectID.isValid(id)){
-        res.status(404).send();
+        return res.status(404).send();
     }
     Todo.findById(id).then((todo) => {
         if(!todo){
-            res.status(404).send();
+            return res.status(404).send();
         }
         res.send({todo});
     }).catch((e) => {
         res.status(400).send();
+    });
+});
+
+//DELETE /todos
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
+        res.send(400).send();
     });
 });
 
