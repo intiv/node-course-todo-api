fca4c684d334cf53d42f2af03d39419f46dddfeb
Inti Velasquez <intiv@unitec.edu>
2018-06-13 20:18:37 -0600

    pls work user

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 let { mongoose } = require('./db/mongoose');
-const { User } = require('./Models/user');
+// const { User } = require('./Models/user');
 const { Todo } = require('./Models/todo');
 const { ObjectID } = require('mongodb');
 
